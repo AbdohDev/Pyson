3a966258186ab0b83c33424f62f23d9a459f9edd
AbdohDev <a2020a20a@gmail.com>
2021-06-16 05:49:35 +0800

    creating a menu

---
+++ menu.py
@@ -0,0 +1,13 @@
+# def main():
+#     menu()
+
+def menu():
+    print("-----welcome to car rent-----")
+    print("[1] Sign in")
+    print("[2] Sign up")
+    print("[3] Guest Login")
+    print("[0] Exit")
+    
+    menu = input("\nSelect an Option: ")
+    
+print (menu())
